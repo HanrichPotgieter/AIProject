0b5ca21187560e90082491558477b7c9f0969d66
Hanrich <Hanrichp@gmail.com>
2015-03-13 16:32:00 +0200

    Playing around with the gui

--- GUI.java
+++ GUI.java
@@ -3,8 +3,9 @@ Created by Isabeel Nel and Hanrich Potgieter
 Date: 13 March 2015
 */
 
-
-import javax.swing.*; 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class GUI
 {
@@ -20,10 +21,19 @@ public class GUI
 	public void creatGUI()
 	{
         JFrame frame = new JFrame("GAME");
+        GridLayout layout = new GridLayout(0,3);
+        frame.setLayout(layout);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+ 		JButton b = new JButton("Just fake button");
+ 		JButton c = new JButton("Just fake button");
+ 		//layout.add(b);
+
         //Add the ubiquitous "Hello World" label.
         JLabel label = new JLabel("This is our game!");
         frame.getContentPane().add(label);
+        frame.getContentPane().add(b);
+        frame.getContentPane().add(c);
         //Display the window.
         frame.pack();
         frame.setVisible(true);
