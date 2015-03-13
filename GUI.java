78c5ab6d69aae8bac98b7ee20b88a9167f3be83f
Hanrich <Hanrichp@gmail.com>
2015-03-13 17:13:42 +0200

    added a nice baglayout

--- GUI.java
+++ GUI.java
@@ -9,6 +9,9 @@ import javax.swing.*;
 
 public class GUI
 {
+	Integer ROW_COUNT = 9;
+	Integer COL_COUNT = 8;
+
 	public GUI()
 	{
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
@@ -21,19 +24,36 @@ public class GUI
 	public void creatGUI()
 	{
         JFrame frame = new JFrame("GAME");
-        GridLayout layout = new GridLayout(0,3);
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
+
         frame.setLayout(layout);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
- 		JButton b = new JButton("Just fake button");
- 		JButton c = new JButton("Just fake button");
- 		//layout.add(b);
 
-        //Add the ubiquitous "Hello World" label.
+
+ 		JButton b = new JButton("Just fake button");
+ 		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		frame.getContentPane().add(b,c);
+ 		JButton button2 = new JButton("Just fake button");
+ 		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+		c.gridy = 0;
+		 frame.getContentPane().add(button2,c);
+        JButton button3 = new JButton("Just fake button");
+ 		c.fill = GridBagConstraints.HORIZONTAL;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.ipady = 40;      //make this component tall
+		c.weightx = 0.0;
+		c.gridwidth = 3;
+		c.gridx = 0;
+		c.gridy = 1;
         JLabel label = new JLabel("This is our game!");
-        frame.getContentPane().add(label);
-        frame.getContentPane().add(b);
-        frame.getContentPane().add(c);
+        //frame.getContentPane().add(label);
+        
+        frame.getContentPane().add(button3,c);
         //Display the window.
         frame.pack();
         frame.setVisible(true);
