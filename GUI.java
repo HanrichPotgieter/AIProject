76077b11e9a8325d45691ba1ddfe42d5e42d5655
Hanrich <Hanrichp@gmail.com>
2015-03-13 16:17:53 +0200

    Silly GUI is working

--- GUI.java
+++ GUI.java
@@ -3,7 +3,29 @@ Created by Isabeel Nel and Hanrich Potgieter
 Date: 13 March 2015
 */
 
+
+import javax.swing.*; 
+
 public class GUI
 {
-	
+	public GUI()
+	{
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                creatGUI();
+            }
+        });
+	}
+
+	public void creatGUI()
+	{
+        JFrame frame = new JFrame("GAME");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("This is our game!");
+        frame.getContentPane().add(label);
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
--- main.java
+++ main.java
@@ -6,5 +6,6 @@ Date: 13 March 2015
 public class main {
 	public static void main(String args[]){
 
+		GUI gui = new GUI();
 	}
 }
