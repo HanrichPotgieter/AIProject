23e463ba03d69946d2174ff4f8d1697b43c867c6
Hanrich <Hanrichp@gmail.com>
2015-03-14 11:26:36 +0200

    New GUI changes... its something

--- GUI.java
+++ GUI.java
@@ -30,8 +30,49 @@ public class GUI
         frame.setLayout(layout);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JButton b = new JButton("Start Game");
+       	c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridheight = 2;
+		frame.getContentPane().add(b,c);
 
+		JRadioButton playerButton = new JRadioButton("Player vs Player");
+    	//playerButton.setActionCommand(birdString);
+    	playerButton.setSelected(true);
+    	c.fill = GridBagConstraints.VERTICAL;
+		c.gridx = 1;
+		c.gridy = 0;
+		c.gridheight = 1;
+		frame.getContentPane().add(playerButton,c);
 
+    	JRadioButton aiButton = new JRadioButton("AI vs AI");
+    	//aiButton.setActionCommand("Ai ");
+    	c.fill = GridBagConstraints.VERTICAL;
+		c.gridx = 1;
+		c.gridy = 1;
+		c.gridheight = 1;
+		frame.getContentPane().add(aiButton,c);
+
+    	ButtonGroup group = new ButtonGroup();
+    	group.add(playerButton);
+    	group.add(aiButton);
+
+    	int rowStart = 3;
+
+    	for(int i = 0;i<ROW_COUNT;i++){
+    		for(int j = 0;j <COL_COUNT;j++){
+    			    c.fill = GridBagConstraints.HORIZONTAL;
+					c.gridx = rowStart + j;
+					c.gridy = i;
+					c.gridheight = 1;
+					JButton btn = new JButton();
+					btn.setBackground(Color.GREEN);
+					frame.getContentPane().add(btn,c);
+    		}
+    	}
+
+		/*
  		JButton b = new JButton("Just fake button");
  		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 0;
@@ -54,6 +95,7 @@ public class GUI
         //frame.getContentPane().add(label);
         
         frame.getContentPane().add(button3,c);
+*/
         //Display the window.
         frame.pack();
         frame.setVisible(true);
