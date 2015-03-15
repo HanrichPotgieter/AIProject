d007513f8701f0a6df2e2d513bd17f07da144a88
Isabel <isabel.nel.isabel@gmail.com>
2015-03-15 18:14:51 +0200

    added GameState

---
+++ AIProject_Game/src/aiproject_game/GameState.java
@@ -0,0 +1,38 @@
+/*
+ *  COS 314 Project 1 
+ *  Authors:
+ *  Isabel Nel : 13070305
+ *  Hanrich Potgieter : 
+ *
+ */
+
+
+package aiproject_game;
+
+public class GameState {
+    // ~~~~~~~~~~ Class variables 
+    public enum states {
+                       Player_A_Turn,
+                       Player_B_Turn,
+                       Player_A_Win,
+                       Player_B_Win
+    };
+   private states currentState;
+    
+    //~~~~~~~~~~ Class constructor
+    GameState() {
+    currentState = states.Player_A_Turn;
+    }
+    
+    //~~~~~~~~~ GetGameState will return the current game state 
+    public states getGameState(){
+        return currentState;
+    }
+    
+    //~~~~~~~~~ setGameState will be used to set the state of the game
+    public void setGameState(states newState){
+        currentState = newState; 
+    }
+    
+    
+}
