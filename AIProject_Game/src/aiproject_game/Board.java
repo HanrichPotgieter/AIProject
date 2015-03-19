b55a577e76bf771b6f78f9a980531ad0657fdfdb
Hanrich <Hanrichp@gmail.com>
2015-03-19 10:22:58 +0200

    Changed new heuristic

--- AIProject_Game/src/aiproject_game/Board.java
+++ AIProject_Game/src/aiproject_game/Board.java
@@ -1082,11 +1082,11 @@ public class Board {
          
         if(gameState.getGameState() ==GameState.states.Player_A_Turn)
         {
-                return counterA - counterB;
+                return (counterA - counterB)*5;
         }
         else if(gameState.getGameState() ==GameState.states.Player_B_Turn)
         {
-                return counterB - counterA;
+                return (counterB - counterA)*5;
         }
         return 0;
      }
@@ -1152,18 +1152,18 @@ public class Board {
             addedToArray =  distanceFromEnemy.add(distance);
           }
           // now get the lowest value in the arrayList = will be the closest cell
-          System.out.println("Size of ditsance array: " + distanceFromEnemy.size());
+          //System.out.println("Size of ditsance array: " + distanceFromEnemy.size());
           if(!distanceFromEnemy.isEmpty()){
               for(int i = 0; i < distanceFromEnemy.size(); i++){
                   if(distanceFromEnemy.get(i) < lowestValue){ // then swop and distance is now the smallest value thus far
-                      System.out.println("lowestValue:" + distanceFromEnemy.get(i));
+                      //System.out.println("lowestValue:" + distanceFromEnemy.get(i));
                    lowestValue = distanceFromEnemy.get(i);
                   }
               }
           }
           
 
-         System.out.print("LowestVal = " + lowestValue );
+         //System.out.print("LowestVal = " + lowestValue );
          return N - lowestValue;
           
 // now distance is the smallest value in the array... now assign a value on how good it is....
